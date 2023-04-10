09f976e9ede9bf8e270cd8f0df88a03a8f23ad60
ArtemKataev <tipa.atlet@mail.ru>
2023-04-10 18:17:39 +0300

    added 3rd task of HW

--- First_Homework/Program.cs
+++ First_Homework/Program.cs
@@ -39,4 +39,18 @@ max=b;
 }
 Console.WriteLine(max);*/
 
+//Part3
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int n;
+n=(number%2);
+if(n==0)
+{
+  Console.WriteLine("even number");
+}
+else 
+{
+  Console.WriteLine("odd number");
+}
+
 
