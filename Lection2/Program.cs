31d591d29daeb495f00e0a3644cb7bedcc18ebac
ArtemKataev <tipa.atlet@mail.ru>
2023-04-13 00:29:35 +0300

    some update

--- Lection2/Program.cs
+++ Lection2/Program.cs
@@ -34,49 +34,49 @@
 // }
 
 
-// void Fillarray(int[] collection)
-// {
-//   int length =collection.Length;
-//   int index =0;
-//   while(index<length)
-//   {
-//     collection[index]=new Random().Next(1,10);
-//     index++;
-//   }
-// }
-// void Printarray(int[] col)
-// {
-// int count =col.Length;
-// int position =0;
-// while(position<count)
-// {
-// Console.WriteLine(col[position]);
-// position++;
-// }
-// }
+void Fillarray(int[] collection)
+{
+  int length =collection.Length;
+  int index =0;
+  while(index<length)
+  {
+    collection[index]=new Random().Next(1,10);
+    index++;
+  }
+}
+void Printarray(int[] col)
+{
+int count =col.Length;
+int position =0;
+while(position<count)
+{
+Console.WriteLine(col[position]);
+position++;
+}
+}
 
-// int IndexOf (int[] collection, int find)
-// {
-//   int count =collection.Length;
-//   int index =0;
-//   int position =-1;
-//   while(index<count)
-//   {
-//     if(collection[index]==find)
-//     {
-// position =index;
-// break;
-//     }
-//     index++;
-//   }
-//   return position;
-// }
-// int [] array=new int [10];
+int IndexOf (int[] collection, int find)
+{
+  int count =collection.Length;
+  int index =0;
+  int position =-1;
+  while(index<count)
+  {
+    if(collection[index]==find)
+    {
+position =index;
+break;
+    }
+    index++;
+  }
+  return position;
+}
+int [] array=new int [10];
 
-// Fillarray(array);
-// array[4]=4;
-// array[6]=4;
-// Printarray(array);
-// Console.WriteLine();
-// int pos =IndexOf(array,1421);
-// Console.WriteLine(pos);
+Fillarray(array);
+array[4]=4;
+array[6]=4;
+Printarray(array);
+Console.WriteLine();
+int pos =IndexOf(array,1);
+Console.WriteLine(pos);
