db910c5cffe421460469c5626a47b23af7263a3a
ArtemKataev <tipa.atlet@mail.ru>
2023-04-15 01:23:02 +0300

    added task 19 and 23

---
+++ HW_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// // TASK19
+// Console.WriteLine("enter number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int n1 = num/10000;
+// int n2 = num/10/10/10%10;
+// int n4 = num%100/10%10;
+// int n5 = num%10;
+
+// if(n1==n5&&n2==n4)
+// {
+// Console.WriteLine("число палиндром");
+// }
+// else
+// {
+// Console.WriteLine("число не палиндром");
+// }
+
+//TASK21
+
+
+
+
+
+
+// TASK23
+// Console.WriteLine("enter number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i=1;
+// while(i<=num)
+// {
+//   Console.WriteLine(i*i*i);
+//   i++;
+// }
