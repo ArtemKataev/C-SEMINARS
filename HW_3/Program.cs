93c1003dbf3aad96ddcfb10a06205842bafb2fa2
ArtemKataev <tipa.atlet@mail.ru>
2023-04-15 16:19:31 +0300

    added task 21 and changed 23

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -16,19 +16,33 @@
 // Console.WriteLine("число не палиндром");
 // }
 
-//TASK21
 
 
+// //TASK21
+// Console.WriteLine("enter number x1");
+// int X1= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter number x2");
+// int X2= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter number x3");
+// int X3= Convert.ToInt32(Console.ReadLine());
 
+// Console.WriteLine("enter number y1");
+// int Y1= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter number y2");
+// int Y2= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter number y3");
+// int Y3= Convert.ToInt32(Console.ReadLine());
+
+// double a =Math.Sqrt(Math.Pow((X1-Y1),2)+(Math.Pow((X2-Y2),2))+(Math.Pow((X3-Y3),2)));
+// Console.WriteLine(a);
 
 
 
 // TASK23
 // Console.WriteLine("enter number");
 // int num = Convert.ToInt32(Console.ReadLine());
-// int i=1;
-// while(i<=num)
+// int i;
+// for(i=1;i<=num;i++)
 // {
-//   Console.WriteLine(i*i*i);
-//   i++;
+//   Console.WriteLine(Math.Pow(i,3));
 // }
