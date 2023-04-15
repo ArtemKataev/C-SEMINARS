5e1b6cbe8d894a41d4d68b23878b41558626d474
ArtemKataev <tipa.atlet@mail.ru>
2023-04-15 16:25:48 +0300

    info in seminar3

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿// Console.WriteLine("enter number 1");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter number 2");
+// int Y = Convert.ToInt32(Console.ReadLine());
+// if(X>0&&Y>0)
+// {
+//   Console.WriteLine("1 part");
+// }
+// else if(X<0&&Y>0)
+// {
+//   Console.WriteLine("2 part");
+// }
+// else if(X<0&&Y<0)
+// {
+//   Console.WriteLine("3 part");
+// }
+// else if(X>0&&Y<0)
+// {
+//   Console.WriteLine("4 part");
+// }
+// else
+// {
+//   Console.WriteLine("0 0 bastard");
+// }
+
+
+
+// switch(X)
+// {
+//   case 1:
+//   {
+// Console.WriteLine("1 part");
+// break;
+//   }
+//   case 2:
+//   {
+//     Console.WriteLine("2 part");
+//   break;
+//   }
+//   case 3:
+//   {
+// Console.WriteLine("3 part");
+// break;
+//   }
+//   case 4:
+//   {
+//     Console.WriteLine("4 part");
+//   break;
+//   }
+//   default:
+//   {
+//     Console.WriteLine("no one part");
+//   break;
+//   }
+// }
