5caee92b6fc9b25aea72d646c15f89312e47c2d6
ArtemKataev <tipa.atlet@mail.ru>
2023-04-25 01:23:08 +0300

    added 3 tasks of HW5

---
+++ HW_5/Program.cs
@@ -0,0 +1,56 @@
+﻿// TASK1
+// int [] array = new int [new Random().Next(10,25)];
+// int summa=0;
+// for(int i =0;i<array.Length;i++)
+// {
+// array[i]= new Random().Next(100,1000);
+// int a=array[i]%2;
+//   if(a==0)
+//   {
+//     summa++;
+//   }
+
+// }
+// Console.WriteLine(summa);
+// Console.WriteLine(String.Join(", ",array));
+
+
+// TASK2
+// int [] array = new int [new Random().Next(-5,11)];
+// int sum =0;
+// for(int i =0;i<array.Length;i++)
+// {
+// array[i]= new Random().Next(1,10);
+// int a=i%2;
+//   if(a!=0)
+//   {
+//   sum+=array[i];
+//   }
+
+// }
+// Console.WriteLine(sum);
+// Console.WriteLine(String.Join(", ",array));
+
+
+// // TASK3
+// int [] array = new int [] {241,4124,2142,123,4,1};
+// int max=array[0];
+// int min=array[0];
+// for(int i=0;i<array.Length;i++)
+// {
+//   if(array[i]>max)
+//   {
+//     max=array[i];
+//   }
+//   else if(array[i]<min)
+//   {
+//     min=array[i];
+//   }
+// }
+// int count =max-min;
+// Console.WriteLine("макс число равно " + max);
+// Console.WriteLine("мин число равно " + min);
+// Console.WriteLine(String.Join(", ",array));
+// Console.WriteLine("в результате получаем " +count);
+
+
