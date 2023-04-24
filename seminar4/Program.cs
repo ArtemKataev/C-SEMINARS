a7f06e130e21d4a0bcdd40a180876e43a9b07758
ArtemKataev <tipa.atlet@mail.ru>
2023-04-24 13:42:27 +0300

    added some tasks

---
+++ seminar4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Console.WriteLine("enter number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count =0;
+// for(int i=1;i<=number;i++)
+// {
+//   count= count+i;
+//   Console.WriteLine(count);
+// }
+
+  // Console.WriteLine("enter number");
+  // int number =Convert.ToInt32(Console.ReadLine());
+  // int sum =0;
+  // int count =0;
+
+  // int GetSumNum()
+  // {
+  //   while(number>count)
+  //   {
+  //     count++;
+  //     sum+=count;
+  //   }
+  //   return sum;
+  // }
+
+  // Console.WriteLine(GetSumNum());
+
+
+
+// int num=0;
+// int NumCount(int num)
+// {
+//   return Convert.ToString(num).Length;
+// }
+//   Console.WriteLine("enter number");
+//   Console.WriteLine(NumCount(Convert.ToInt32(Console.ReadLine()))); 
+ 
+
+// int [] numArr= new int [8];
+// for(int i=0;i<numArr.Length;i++)
+// {
+//   numArr[i] = new Random().Next(0,2);
+// }
+
+// Console.WriteLine(String.Join(" ",numArr));
+// Console.WriteLine(numArr.Length);
