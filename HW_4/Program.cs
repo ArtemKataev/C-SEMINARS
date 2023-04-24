39182d4a8c2dd248b70b670ee1c1873b89a31836
ArtemKataev <tipa.atlet@mail.ru>
2023-04-24 14:12:50 +0300

    added task 1 and task2

---
+++ HW_4/Program.cs
@@ -0,0 +1,41 @@
+﻿//                                                 Task1
+// Console.WriteLine("enter number 1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter number 2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int count =num1;
+
+// int GetSumNum()
+//   {
+//     for(int i=1;i<num2;i++)
+//     {
+//       count=count*num1;
+     
+//     }
+//      return count;
+//   }
+
+// int Count1= GetSumNum();
+// Console.WriteLine(Count1);
+
+//                               // Task2
+
+// Console.WriteLine("enter number 1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// int AllSum()
+// {
+// while(num1>0)
+// {
+// int num= num1%10;
+// num1 =num1/10;
+// sum=sum+num;
+// }
+// return sum;
+// }
+// int SumCost =AllSum();
+// Console.WriteLine(SumCost);
+
+
+           // TASK3
+
