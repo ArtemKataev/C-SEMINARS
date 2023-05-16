7ad3ccf24656008eee2ebfc91202975782eb1196
ArtemKataev <tipa.atlet@mail.ru>
2023-05-16 17:59:29 +0300

    added sem6

---
+++ seminar6/Program.cs
@@ -0,0 +1,65 @@
+﻿// Console.WriteLine("enter 1 number");
+// int a=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter 2 number");
+// int b=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("enter 3 number");
+// int c=Convert.ToInt32(Console.ReadLine());
+
+// if(a+b>c&&a+c>b&&b+c>a)
+// {
+//   Console.WriteLine("thats right");
+// }
+// else if(a<=0&&b<=0&&c<=0)
+// {
+//   Console.WriteLine("you have a mistake");
+// }
+// else
+// {
+//   Console.WriteLine("you have a mistake");
+// }
+
+
+// Console.WriteLine("enter number");
+// int num =Convert.ToInt32(Console.ReadLine());
+// string result="";
+// while(num>0)
+// {
+//   result=num%2+result;
+//   num=num/2;
+// }
+// Console.WriteLine(result);
+
+
+
+// Console.WriteLine("enter number");
+// int num =Convert.ToInt32(Console.ReadLine());
+// int i=2;
+// if(num>1)
+//   {int [] arr = new int [num];
+// arr[0]=0;
+// arr[1]=1;
+//     while(i<num)
+// {
+//   arr[i]=arr[i-1]+arr[i-2];
+//   i++;
+// }
+// Console.WriteLine(String.Join(", ",arr));
+// }
+// else
+// {
+//   Console.WriteLine("try again");
+// }
+
+
+
+
+// int [] array = new int [] {1,2,3,4,5};
+// for(int i=0;i<array.Length/2;i++)
+// {
+  
+//  int temp=array[i];
+//  array[i]=array[array.Length-1-i];
+//  array[array.Length-1-i]=temp;
+ 
+// }
+// Console.WriteLine(String.Join(", ",array));
